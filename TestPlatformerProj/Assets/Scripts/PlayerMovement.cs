a12bca4f16f5ea33431b777b2cb3fd3563ba0ff3
asm-nachetanya <83469217+asm-nachetanya@users.noreply.github.com>
2021-05-12 14:10:53 -0400

    Finished Jumping Mechanic
    Added descp.

---
+++ TestPlatformerProj/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    // Jumping
+    public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    // Movement x Axis
+    float mx;
+
+
+    // Update is called once per frame, checks button presses
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    // Allows movement on x axis
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    // Allows jumping on y axis
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    // Checks if feet touching ground
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if(groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
